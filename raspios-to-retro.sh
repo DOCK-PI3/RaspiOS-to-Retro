be5dce5ae4c043e7079af3f9b9a58b1cb827616a
Mabedeep <41844169+DOCK-PI3@users.noreply.github.com>
2026-03-14 23:42:18 +0000

    Add files via upload

--- raspios-to-retro.sh
+++ raspios-to-retro.sh
@@ -55,7 +55,7 @@ export CXXFLAGS="-march=armv8-a+crc+simd -O3"
 echo "Configurando compilación para RPi 5 (KMS/Vulkan)..."
 # Optimizaciones específicas para RPi 5 y desactivación de X11
 #./configure --enable-vulkan --enable-kms --enable-egl --enable-udev --enable-alsa --enable-ssl --disable-x11 --disable-wayland
-./configure --enable-kms --enable-egl --enable-vulkan --disable-neon --disable-sdl --enable-sdl2 --disable-oss --enable-x11 --enable-wayland --disable-al --disable-jack --disable-qt --enable-builtinmbedtls
+./configure --enable-kms --enable-egl --enable-vulkan --enable-udev --disable-neon --disable-sdl --enable-sdl2 --disable-oss --enable-x11 --enable-wayland --disable-al --disable-jack --disable-qt --enable-builtinmbedtls
 echo "Compilando (esto puede tardar unos minutos)..."
 make -j$(nproc)
 
